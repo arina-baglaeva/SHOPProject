95c0f6bfbf1e2e6a465fb459e682aabfcc63c905
ariba <ariba2576@gmail.com>
2026-02-07 01:34:47 +0300

    Shop app4_ex hm-18_list done

--- src/org/skypro/skyshop/searchable/SearchEngine.java
+++ src/org/skypro/skyshop/searchable/SearchEngine.java
@@ -1,55 +1,42 @@
 package org.skypro.skyshop.searchable;
 
-public class SearchEngine {
-    public Searchable[] arrOfAll;
+import java.util.*;
 
-    public SearchEngine(int n) {
-        arrOfAll = new Searchable[n];
+public class SearchEngine {
+    public List<Searchable> arrOfAll;
+
+    public SearchEngine() {
+        arrOfAll = new LinkedList<>();
     }
 
-    public Searchable[] search(String s) {
-        Searchable[] arr = new Searchable[5];
-        int c = 0;
-        for (int i = 0; i < arrOfAll.length; i++) {
-            if (arrOfAll[i] != null && arrOfAll[i].searchTerm().toLowerCase().contains(s.toLowerCase())) {
-                arr[c] = arrOfAll[i];
-                c++;
+    public List<Searchable> search(String s) {
+        List<Searchable> arr = new ArrayList<>();
+        for (Searchable searchable : arrOfAll) {
+            if (searchable != null && searchable.searchTerm().toLowerCase().contains(s.toLowerCase())) {
+                arr.add(searchable);
             }
-            if (c > 4)
-                break;
         }
         System.out.println("Поиск по списку: ");
         return arr;
     }
 
     public void add(Searchable obj) {
-        boolean f = false;
-        for (int i = 0; i < arrOfAll.length; i++) {
-            if (arrOfAll[i] == null) {
-                arrOfAll[i] = obj;
-                f = true;
-                break;
-            }
-        }
-        if (f) {
-            System.out.println("Добавлено в список: " + obj.getOfNameObject());
-        } else {
-            System.out.println("Нет мест в списке!");
-        }
+        arrOfAll.add(obj);
+        System.out.println("Добавлено в список: " + obj.getOfNameObject());
     }
 
     public Searchable findBestMatch(String s) throws BestResultNotFound {
-        int[] counts = new int[arrOfAll.length];
+        int[] counts = new int[arrOfAll.size()];
         int ind = 0;
         int count = 0;
         int mx = 0;
-        for (int i = 0; i < arrOfAll.length; i++) {
-            if (arrOfAll[i] != null) {
-                int ind_s = arrOfAll[i].searchTerm().indexOf(s, ind);
+        for (int i = 0; i < arrOfAll.size(); i++) {
+            if (arrOfAll.get(i) != null) {
+                int ind_s = arrOfAll.get(i).searchTerm().indexOf(s, ind);
                 while (ind_s != -1) {
                     count++;
                     ind = ind_s + s.length();
-                    ind_s = arrOfAll[i].searchTerm().indexOf(s, ind);
+                    ind_s = arrOfAll.get(i).searchTerm().indexOf(s, ind);
                 }
                 counts[i] = count;
                 if (count > counts[mx]) mx = i;
@@ -59,7 +46,7 @@ public class SearchEngine {
         if (counts[mx] == 0) {
             throw new BestResultNotFound("Для запроса: " + s + " - не нашлось подходящей статьи.");
         }
-        return arrOfAll[mx];
+        return arrOfAll.get(mx);
 
     }
 }
